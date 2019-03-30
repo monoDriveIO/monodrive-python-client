5658e0391818b02fcffbaf2dfc8618a7343a0b81
Brad Null <brad@monodrive.io>
2019-03-30 07:10:47 -0500

    Adding some documentation

--- monodrive/client.py
+++ monodrive/client.py
@@ -1,21 +1,46 @@
+"""Client.py
+Client to connect to the monodrive simulator.
+"""
 import socket
 
 
 class Client:
+    """Client to connect to the monodrive simulator"""
 
     def __init__(self, ip, port):
+        """ Constructor.
+
+        Args:
+            ip(str):  The IP address of the simulator
+            port(str): The port for the simulator
+        """
         self.ip = ip
         self.port = port
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.connected = False
 
     def connect(self):
-        print("connecting to {0}:{1}".format(self.ip, self.port))
-        self.sock.connect((self.ip, self.port))
+        """ Establish a connection with the server.
+
+        Returns:
+            True if the connection was established.
+        """
+        self.connected = self.sock.connect((self.ip, self.port))
+        return self.connected
 
     def disconnect(self):
+        """Disconnect from the server."""
         self.sock.close()
 
     def read(self, length):
+        """Read data from the server.
+
+        Args:
+            length(int): The length, in bytes, of data to read
+
+        Returns:
+            The binary data that was read from the server.
+        """
         data = b''
         count = 0
         while count < length:
@@ -29,4 +54,10 @@ class Client:
         return data
 
     def write(self, message):
+        """Write data to the server.
+
+        Args:
+            message(bytearray): The binary message to write to the server
+
+        """
         self.sock.sendall(message)
