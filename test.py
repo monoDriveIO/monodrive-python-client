84816c47a611b9130cb179ddb7baa1226b0edd59
Brad Null <brad@bradleynull.com>
2019-09-06 09:16:13 -0500

    Cleaning up test cases

--- test.py
+++ test.py
@@ -11,6 +11,17 @@ from itertools import combinations
 from monodrive.simulator import Simulator
 from uut.vehicles.example_vehicle import ExampleVehicle
 
+# Flag to allow user to stop the simulation from SIGINT
+RUNNING = True
+
+def handler(signum, frame):
+    """"Signal handler to turn off the simulator with ctl+c"""
+    global RUNNING
+    RUNNING = False
+
+
+signal.signal(signal.SIGINT, handler)
+
 
 def build_sensor_config(sensors: list, base_dir: str = "./sensors/") -> dict:
     sensor_json = "["
@@ -23,77 +34,73 @@ def build_sensor_config(sensors: list, base_dir: str = "./sensors/") -> dict:
 
     return json.loads(sensor_json)
 
+def run_trial(sim_config: dict, sensor_config: dict, trajectory: dict,
+              weather: dict) -> dict:
+    global RUNNING
+
+    # configure this simulator client
+    # Load the reporting sensor configuration and software under test
+    simulator = Simulator(sim_config, trajectory)
+
+    # Start the simulation
+    simulator.start()
+
+    vehicle = ExampleVehicle(sim_config, sensor_config)
+    vehicle.start()
+
+    # Start stepping the simulator
+    start_time = time.time()
+    frame_times = []
+
+    # Run through every step in the trajectory
+    for i in range(0, len(trajectory) - 1):
+        frame_start = time.time()
+        response = vehicle.step()
+        frame_times.append(time.time() - frame_start)
+        if RUNNING is False:
+            break
+
+    total_time = time.time() - start_time
+    stats = dict()
+    stats["total_time"] = total_time
+    stats["fps"] = frame_times
+    stats["avg_fps"] = 1.0 / np.mean(np.array(frame_times))
+
+    simulator.stop()
+    vehicle.stop()
+
+    return stats
+
 
 if __name__ == "__main__":
     root = os.path.dirname(__file__)
 
-    # Flag to allow user to stop the simulation from SIGINT
-    running = True
-
-    def handler(signum, frame):
-        """"Signal handler to turn off the simulator with ctl+c"""
-        global running
-        running = False
-    signal.signal(signal.SIGINT, handler)
-
-
     # Load the sensor configuration and software under test
     avail_sensors = ["Camera", "Collision", "GPS", "IMU", "Lidar", "Radar",
                      "RPM", "State"]
+    # Load the trajectory and simulator configurations
+    trajectory = json.load(open(os.path.join(root, 'configurations',
+                                             'trajectories',
+                                             'HighWayExitReplay.json')))
+    sim_config = json.load(open(os.path.join(root, 'configurations',
+                                             'simulator.json')))
+
+    # Load and configure the weather conditions for the simulator
+    weather = json.load(
+        open(os.path.join(root, 'configurations', 'weather.json')))
+    profile = weather['profiles'][10]
+    profile['id'] = 'test'
 
     count = 0
     for i in range(1, len(avail_sensors)+1):
         for combo in combinations(avail_sensors, i):
-            # Load the trajectory and simulator configurations
-            trajectory = json.load(open(os.path.join(root, 'configurations',
-                                                     'trajectories',
-                                                     'HighWayExitReplay.json')))
-            sim_config = json.load(open(os.path.join(root, 'configurations',
-                                                     'simulator.json')))
-
-            # configure this simulator client
-            # Load the reporting sensor configuration and software under test
-            simulator = Simulator(sim_config, trajectory)
-
-            # Load and configure the weather conditions for the simulator
-            weather = json.load(
-                open(os.path.join(root, 'configurations', 'weather.json')))
-            profile = weather['profiles'][10]
-            profile['id'] = 'test'
-
-            # Start the simulation
-            simulator.start()
-
             sensor_config = build_sensor_config(
-                combo, base_dir=os.path.join(root, "sensors"))
+                list(combo), base_dir=os.path.join(root, "sensors"))
+            stats = run_trial(sim_config, sensor_config, trajectory, weather)
 
-            vehicle = ExampleVehicle(sim_config, sensor_config)
-            vehicle.start()
-            print(vehicle.sensors_ids)
-
-            # Start stepping the simulator
-            start_time = time.time()
-            frame_times = []
-            for i in range(0, len(trajectory)-1):
-                frame_start = time.time()
-                response = vehicle.step()
-                frame_times.append(time.time() - frame_start)
-                if running is False:
-                    break
-            total_time = time.time() - start_time
-            stats = dict()
-            stats["total_time"] = total_time
-            stats["fps"] = frame_times
-            stats["avg_fps"] = 1.0/np.mean(np.array(frame_times))
-            sensor_config.append(stats)
-            with open("trial" + str(count) + ".json", "w") as f:
+            with open("trial" + str(count).zfill(3) + ".json", "w") as f:
                 f.write(json.dumps(sensor_config))
-            count +=1
+            count += 1
 
-            print("Stopping the simulator.")
-            simulator.stop()
-            print("Stopping the uut.")
-            vehicle.stop()
-
-            if running is False:
+            if RUNNING is False:
                 break
