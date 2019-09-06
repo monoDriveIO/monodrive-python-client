6be3ab38c2b8680a9d12a65d9961e5e0ea37a04b
Graciela Gonzalez <gmtlali88@gmail.com>
2019-09-05 23:08:00 -0500

    Create a util script to send the summary to the mongodb

---
+++ monodrive/utils.py
@@ -0,0 +1,17 @@
+import pymongo
+
+
+def send_to_summary_to_mongodb(summary):
+    # init client connection
+    host = '192.168.1.118'
+    port = 27017
+    client = pymongo.MongoClient(host, port)
+    db = client['monodrive']
+    collection = db['report_summary']
+    doc = {
+        'id': 'Report Summary',
+        'data': summary,
+    }
+    res = collection.insert_one(doc)
+    doc_id = res.inserted_id
+    print('stored doc with id: {}'.format(doc_id))
