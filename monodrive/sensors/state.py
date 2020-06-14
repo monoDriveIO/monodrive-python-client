27e6973e499cb38c487a38de497fcba5adceb404
Devin A. Conley <devinaconley@gmail.com>
2020-06-14 14:29:44 -0500

    update state sensor for new format

--- monodrive/sensors/state.py
+++ monodrive/sensors/state.py
@@ -11,30 +11,43 @@ from monodrive.sensors import Sensor, DataFrame
 
 
 @objectfactory.Factory.register_class
-class StateFrameWheel(objectfactory.Serializable):
-    """Data model for state sensor frame item wheel"""
-    wheel_id = objectfactory.Field(name="id")
-    orientation = objectfactory.Field()
+class StateFrameObject(objectfactory.Serializable):
+    """Data model for state sensor frame object"""
+    name = objectfactory.Field()
+    odometry = objectfactory.Field()
+    tags = objectfactory.Field()
+    oobbs = objectfactory.Field()
 
 
 @objectfactory.Factory.register_class
-class StateFrameItem(objectfactory.Serializable):
-    """Data model for state sensor frame item"""
-    name = objectfactory.Field()
-    tags = objectfactory.Field()
-    velocity = objectfactory.Field()
-    wheel_speed = objectfactory.Field()
-    position = objectfactory.Field()
-    angular_velocity = objectfactory.Field()
+class StateFrameWheel(objectfactory.Serializable):
+    """Data model for state sensor frame vehicle wheel"""
+    wheel_id = objectfactory.Field(name="id")
+    speed = objectfactory.Field()
+    pose = objectfactory.Field()
+
+
+@objectfactory.Factory.register_class
+class StateFrameVehicle(objectfactory.Serializable):
+    """Data model for state sensor frame vehicle"""
+    state = objectfactory.Nested(field_type=StateFrameObject)
     wheels = objectfactory.List(field_type=StateFrameWheel)
+
+
+@objectfactory.Factory.register_class
+class StateFrameData(objectfactory.Serializable):
+    """Data model for state sensor frame object data"""
+    objects = objectfactory.List(field_type=StateFrameObject)
+    vehicles = objectfactory.List(field_type=StateFrameVehicle)
 
 
 @objectfactory.Factory.register_class
 class StateFrame(DataFrame, objectfactory.Serializable):
     sensor_id = objectfactory.Field()
-    timestamp = objectfactory.Field()
+    time = objectfactory.Field()
     game_time = objectfactory.Field()
-    object_list = objectfactory.List(name='frame', field_type=StateFrameItem)
+    sample_count = objectfactory.Field()
+    frame = objectfactory.Nested(field_type=StateFrameData)
 
 
 @objectfactory.Factory.register_class
